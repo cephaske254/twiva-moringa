7bbd0d201cd5351d537215fd26d3a8aa0279655a
cephaske254 <cephaske254@gmail.com>
2022-02-26 05:03:23 +0300

    create context processor for navigation & sidebar

---
+++ utils/requests/movies.py
@@ -0,0 +1,5 @@
+from utils.tmdb import tmdb
+
+
+def get_latest():
+    pass
--- utils/tmdb.py
+++ utils/tmdb.py
@@ -1,21 +1,12 @@
-from sys import api_version
-
 import requests
 
-
-class _RequestConfig:
-    api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjY3NTQ4NzgzZWViYzA0YWM5NzY5ZDYxNzY4NTA3MSIsInN1YiI6IjVlYTAwZDU5YmU0YjM2MDAxYzU4ZTVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l1IrV4PcLQDkKHqWLWSJU1HsrY_om_SPW8uMTxIK2HY"
-    session: requests.Session
-
-    def __init__(self) -> requests.Session:
-        self.session = requests.Session()
-        self.session.headers = {
-            **self.session.headers,
-            "Authorization": "Bearer %s" % self.api_key,
-            "Content-Type": "application/json;charset=utf-8",
-        }
-
-        return self.session
+tmdb = requests.Session()
+_api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjY3NTQ4NzgzZWViYzA0YWM5NzY5ZDYxNzY4NTA3MSIsInN1YiI6IjVlYTAwZDU5YmU0YjM2MDAxYzU4ZTVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l1IrV4PcLQDkKHqWLWSJU1HsrY_om_SPW8uMTxIK2HY"
 
 
-tmdb = _RequestConfig()
+tmdb.headers.update(
+    {
+        "Authorization": "Bearer %s" % _api_key,
+        "Content-Type": "application/json;charset=utf-8",
+    }
+)
